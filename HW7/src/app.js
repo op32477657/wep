55a00e335bc6b4812be34e064b0755313740edee
op32477657 <100269224+op32477657@users.noreply.github.com>
2022-06-12 16:52:47 +0800

    Add files via upload

---
+++ HW7/src/app.js
@@ -0,0 +1,18 @@
+import './App.css';
+import login from './Components/Login';
+import username from './Components/UserName';
+import password from './Components/Password';
+import button from './Components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='login'> {login()} </div>
+      <div className='input'> {username()} </div>
+      <div className='input'> {password()} </div>
+      <div className='button'> {button()} </div>
+    </div>
+  );
+}
+
+export default App;
