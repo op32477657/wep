ee1c6bc56e255ef3a93daca1154f5fdc3a613358
op32477657 <100269224+op32477657@users.noreply.github.com>
2022-06-20 18:49:13 +0800

    Update Profile.js

--- HW10/src/Profile.js
+++ HW10/src/Profile.js
@@ -11,54 +11,32 @@ const App = () => {
 
     useEffect(() => {
         fetch(url)
-            .then(res => res.json())
-            .then(json => setData(json))
+        .then(res => res.json())
+        .then(json => setData(json))
     }, [])
 
-    return ( <
-        div className = 'all' >
-        <
-        Avatar src = { data.avatar_url }
-        sx = {
-            { width: 250, height: 250 } }
-        /> <
-        Header style = {
-            { fontSize: 30, fontWeight: "bolder" } } > { data.name } < /Header> <
-        Header style = {
-            { fontSize: 18, color: '#828282' } } > { data.login } < /Header> <
-        button > Edit profile < /button>
+    return (
+        <div className='all'>
+            <Avatar src={data.avatar_url} sx={{ width: 250, height:250 }}/>
+            <Header style={{fontSize:30, fontWeight:"bolder"}}> {data.name} </Header>
+            <Header style={{fontSize:18 ,color:'#828282'}}> {data.login} </Header>
+            <button> Edit profile </button>
 
-        <
-        div >
-        <
-        PeopleOutline style = {
-            { color: '#828282' } }
-        />&nbsp; { data.followers } <
-        span style = {
-            { color: '#828282' } } > followers• < /span> { data.following } & nbsp; <
-        span style = {
-            { color: '#828282' } } > following• < /span> <
-        StarOutline style = {
-            { color: '#828282' } }
-        /> { data.public_gists } <
-        /div>
+            <div>
+                <PeopleOutline style={{color:'#828282'}}/>&nbsp;
+                {data.followers}
+                <span style={{color:'#828282'}}> followers •</span>
+                {data.following}&nbsp;
+                <span style={{color:'#828282'}}> following •</span>
+                <StarOutline style={{color:'#828282'}}/>
+                {data.public_gists}
+            </div>
 
-        <
-        div style = {
-            { marginTop: "20px" } } > < PlaceOutlined style = {
-            { color: '#828282' } }
-        />&nbsp;{data.location} </div >
-        <
-        div > < MailOutline style = {
-            { color: '#828282' } }
-        />&nbsp;{data.html_url} </div >
-        <
-        div > < InsertLinkOutlined style = {
-            { color: '#828282' } }
-        />&nbsp;{data.blog} </div >
-
-        <
-        /div>
+            <div style={{marginTop:"20px"}}> <PlaceOutlined style={{color:'#828282'}}/>&nbsp;{data.location} </div>
+            <div> <MailOutline style={{color:'#828282'}}/>&nbsp;{data.html_url} </div>
+            <div> <InsertLinkOutlined style={{color:'#828282'}}/>&nbsp;{data.blog} </div>
+            
+        </div>
     )
 }
 export default App
